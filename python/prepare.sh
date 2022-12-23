f38f22c551e247e2d7c9c8e6894cf9addf324aec
lash <dev@holbrook.no>
2022-12-23 06:23:10 +0000

    Use better supported virtualenv init command

--- python/prepare.sh
+++ python/prepare.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
+venv=1
 if [ ! -d .venv ]; then
-	virtualenv .venv
+	python3 -m venv .venv
+	if [ $? -ne "0" ]; then
+		>&2 echo "venv module doesn't seem to exiss"
+		venv=0
+	fi
 fi
 
-venv=1
-. .venv/bin/activate
-if [ $? -ne "0" ]; then
-	>&2 echo "virtualenv doesn't seem to exiss"
-	venv=0
+if [ $venv -eq 1 ]; then
+	. .venv/bin/activate
+	if [ $? -ne "0" ]; then
+		>&2 echo "virtualenv doesn't seem to exiss"
+		venv=0
+	fi
 fi
 
 pip install -r requirements.txt
 pip install -r test_requirements.txt
 
-if [ $venv == "1" ]; then
+if [ $venv -eq "1" ]; then
 	deactivate
 fi
